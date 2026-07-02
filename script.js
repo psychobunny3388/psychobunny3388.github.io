3f47cbd220b3be91837d8fd1b1c717884a7317e8
psychobunny3388 <danielguogithub3388@gmail.com>
2026-07-02 16:12:37 -0400

    Pokemon Clicker

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let money=0;
 const images=["https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/640px-0001Bulbasaur.png", "https://archives.bulbagarden.net/media/upload/thumb/8/81/0002Ivysaur.png/640px-0002Ivysaur.png","https://img.pokemondb.net/sprites/scarlet-violet/normal/venusaur.png","https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Charmander.png/250px-Charmander.png","https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/detail/005.png"];
 let imageIndex=0;
+const btn=document.querySelector(".moneybutton");
+const moneyDisplay=document.getElementById("moneydisplay");
+const pokemonImage=document.getElementById("pokemonImage");
 function changemoney()
 {
-money+=1;
-console.log("Clicks: ",money);
-
-
+money++;
+moneyDisplay.textContent=money;
+console.log("Money: ",money);
 }
-const btn =document.querySelector("moneybutton");
-btn.addEventListener("click", changemoney())
+btn.addEventListener("click",changemoney);
